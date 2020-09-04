e4a6fb9d1f2df0e64883da1ce43b7d6cd011915a
YuHarada <54430987+Yu0727Harada@users.noreply.github.com>
2020-09-04 09:10:16 +0900

    ABC021 D solved

---
+++ ABC/ABC021/D.cpp
@@ -0,0 +1,120 @@
+//
+// Created on 2020/09/04.
+//
+
+
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <ios>
+#include <iomanip>
+#include <numeric>
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+typedef long long ll;
+using namespace std;
+const long long LINF =1e18;
+const int INF = 1e9;
+
+const int MAX = 510000;//nCkのn,kの取りうるMAX
+const int MOD = 1000000007;
+const int mod = 1000000007;
+long long fac[MAX],finv[MAX],inv[MAX];
+
+//階乗のテーブルを作る
+void COM_initialize(){
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2 ; i < MAX ; i++) {
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD -inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+//二項係数計算,nCkのn,kを引数として渡す
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD)% MOD;
+}
+
+struct mint {
+    ll x; // typedef long long ll;
+    mint(ll x=0):x((x%mod+mod)%mod){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += mod-a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator*=(const mint a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res+=a;
+    }
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res-=a;
+    }
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res*=a;
+    }
+    mint pow(ll t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+        if (t&1) a *= *this;
+        return a;
+    }
+
+    // for prime mod
+    mint inv() const {
+        return pow(mod-2);
+    }
+    mint& operator/=(const mint a) {
+        return (*this) *= a.inv();
+    }
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res/=a;
+    }
+};
+
+int main() {
+    int n,k;
+    cin>>n>>k;
+    COM_initialize();
+    mint ans;
+    for (int i = 1; i <= k; ++i) {
+        ans += COM(n,i) * COM(k-1,(i-1));
+    }
+    cout<<ans.x<<endl;
+    return 0;
+}
