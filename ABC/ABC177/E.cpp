c125a5d04b6cf50679cbe08c7bf4c25eec4377bf
YuHarada <54430987+Yu0727Harada@users.noreply.github.com>
2020-09-13 10:46:46 +0900

    ABC177 E solved

--- ABC/ABC177/E.cpp
+++ ABC/ABC177/E.cpp
@@ -34,9 +34,69 @@ using namespace std;
 const long long LINF =1e18;
 const int INF = 1e9;
 
+vector<pair<ll,ll>>prime_factorize(ll n){
+    vector<pair<ll,ll>>ret;
+    for (ll i = 2; i * i < n ; ++i) {
+        if(n % i == 0){
+            ll ex = 0;//指数
+            while(n % i == 0){
+                ex++;
+                n /= i;
+            }
+            ret.push_back({i, ex});
+        }
+    }
+    if(n != 1){
+        ret.push_back({n,1});
+    }
+    return ret;
+    //example n = 12, ret = [[2,2],[3,1]と返す
+}
 
+ll gcd(ll a,ll b){
+    if(b == 0){
+        return a;
+    }
+    return gcd(b,a %b);
+}
 
 int main() {
-    int
+    int n;
+    cin>>n;
+
+    set<int>s;
+
+    ll init;
+    cin>>init;
+    auto ret = prime_factorize(init);
+    for (int i = 0; i < ret.size(); ++i) {
+        s.insert(ret[i].first);
+    }
+    int sum = init;
+    bool pairwise = true;
+    for (int i = 1; i < n; ++i) {
+        int a;
+        cin>>a;
+        sum = gcd(sum,a);
+        if(pairwise){
+            ret = prime_factorize(a);
+            for (int i = 0; i < ret.size(); ++i) {
+                if(s.count(ret[i].first) != 0){
+                    pairwise = false;
+                }else{
+                    s.insert(ret[i].first);
+                }
+            }
+        }
+    }
+
+    if(sum == 1 && pairwise){
+        cout<<"pairwise coprime"<<endl;
+    }else if(sum == 1){
+        cout<<"setwise coprime"<<endl;
+    }else{
+        cout<<"not coprime"<<endl;
+    }
+
     return 0;
 }
