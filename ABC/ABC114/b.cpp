b8d0704710dc3dee213e605c4f6730ffdcf04abd
YuHarada <54430987+Yu0727Harada@users.noreply.github.com>
2020-10-16 22:27:55 +0900

    ABC114 B solved

---
+++ ABC/ABC114/b.cpp
@@ -0,0 +1,51 @@
+//
+// Created on 2020/10/16.
+//
+
+
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <ios>
+#include <iomanip>
+#include <numeric>
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+typedef long long ll;
+using namespace std;
+const long long LINF =1e18;
+const int INF = 1e9;
+
+
+
+int main() {
+    string s;
+    cin>>s;
+    int ans = INF;
+    for (int i = 0; i + 2 < s.size(); ++i) {
+        int j = i+ 1;
+        int k = j + 1;
+        int temp = (s[i]-'0')*100+(s[j]-'0')*10+(s[k]-'0');
+        chmin(ans,abs(temp - 753));
+    }
+    cout<<ans<<endl;
+    return 0;
+}
