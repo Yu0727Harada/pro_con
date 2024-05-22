d0b9c6e76f2654c09d7ac61a5e63668e27ef9403
yuha2114 <minamigame2718@icloud.com>
2024-05-22 12:50:22 +0900

    solve

---
+++ ABC/ABC345/c.cpp
@@ -0,0 +1,61 @@
+//
+// Created by yu on 2024/05/21.
+//
+
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <math.h>
+#include <ios>
+#include <iomanip>
+#include <numeric>
+//#include <atcoder/all>
+
+
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+typedef long long ll;
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+const long long LINF =1e18;
+const int INF = 1e9;
+
+
+
+int main() {
+    string s;
+    cin>>s;
+    map<char,ll>mp;
+    ll cnt = 0;
+    bool ok = false;
+    for (int i = 0; i < s.size(); ++i) {
+        mp[s[i]]++;
+
+    }
+    cnt = ((ll)s.size() * ((ll)s.size() - 1))/2;
+    for(auto item:mp){
+        cnt -= (item.second * (item.second - 1))/2;
+        if(item.second > 1)ok = true;
+    }
+    if(ok)cnt++;
+        cout << cnt << endl;
+    return 0;
+}
